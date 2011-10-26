9a986dd8b02b14772d3e5248ec9416b956a33396
César Salazar <i.am@cesarsalazar.mx>
2011-10-26 14:08:07 -0500

    Mentores, programa y lugar

--- app.rb
+++ app.rb
@@ -10,6 +10,30 @@ get '/' do
   haml :index
 end
 
+get '/programa' do
+  haml :programa
+end
+
+get '/participantes' do
+  haml :participantes
+end
+
+get '/mentores' do
+  haml :mentores
+end
+
+get '/lugar' do
+  haml :lugar
+end
+
+get '/inscripciones' do
+  haml :inscripciones
+end
+
+get '/equipo' do
+  haml :equipo
+end
+
 get '/stylesheets/*' do
   content_type 'text/css'
   sass '../styles/'.concat(params[:splat].join.chomp('.css')).to_sym
