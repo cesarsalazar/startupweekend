813cb06181500afa62faa6d1e876b6494a31d578
Jeduan Cornejo <jeduan@gmail.com>
2013-01-15 03:23:38 -0600

    Quita info de sw4, cambios esteticos al JS

--- public/javascripts/new.js
+++ public/javascripts/new.js
@@ -1,43 +1,34 @@
 $(function(){
-  
-  $('#menu a').click(function(event){
-    var el = $(this);
-    var target = $( el.attr('href') );
-    var scroll = target.position().top - 100;
-    $('body').animate( { scrollTop: scroll }, 1000 );
+
+  $('#menu a').on('click', function(event){
     event.preventDefault();
+    var target = $( event.currentTarget.getAttribute('href') );
+    var scroll = target.offset().top - 100;
+    $('body').animate( { scrollTop: scroll }, 1000 );
   })
-  
-  var up = $('#bg-up');
-  var horz1 = $('#bg-float');
-  var left1 = $('#bg-right');
-  var right1 = $('#bg-left');
-  var horz2 = $('#bg-float2');
-  var left2 = $('#bg-right2');
-  var right2 = $('#bg-left2');
-  
-  up.css('top',40+'px');
-  horz1.css('top',500+'px');
-  left1.css('top',900+'px');
-  right1.css('top',1300+'px');
-  horz2.css('top',1800+'px');
-  left2.css('top',1800+'px');
-  right2.css('top',2300+'px');
-  
+
+  var up = $('#bg-up').css('top','40px');
+  var horz1 = $('#bg-float').css('top','500px');
+  var left1 = $('#bg-right').css('top','900px');
+  var right1 = $('#bg-left').css('top','1300px');
+  var horz2 = $('#bg-float2').css('top','1800px');
+  var left2 = $('#bg-right2').css('top','1800px');
+  var right2 = $('#bg-left2').css('top','2300px');
+
   var period = 0;
-  $(window).bind('scroll',function(e){
-      parallaxScroll();
+  $(window).on('scroll',function(e){
+    parallaxScroll();
   });
 
-  function parallaxScroll(){
-      var scrolled = $(window).scrollTop();
-      up.css('top',(40-(scrolled*.15))+'px');
-      horz1.css('top',(500-(scrolled*.35))+'px');
-      left1.css('top',(900-(scrolled*.60))+'px');
-      right1.css('top',(1300-(scrolled*.85))+'px');
-      horz2.css('top',(1800-(scrolled*1))+'px');
-      left2.css('top',(1800-(scrolled*.10))+'px');
-      right2.css('top',(2300-(scrolled*.70))+'px');
+  var parallaxScroll = function () {
+    var scrolled = $(window).scrollTop();
+    up.css('top',(40-(scrolled*.15))+'px');
+    horz1.css('top',(500-(scrolled*.35))+'px');
+    left1.css('top',(900-(scrolled*.60))+'px');
+    right1.css('top',(1300-(scrolled*.85))+'px');
+    horz2.css('top',(1800-(scrolled*1))+'px');
+    left2.css('top',(1800-(scrolled*.10))+'px');
+    right2.css('top',(2300-(scrolled*.70))+'px');
   }
-  
-})
+
+});
